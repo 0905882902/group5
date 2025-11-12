e211fc322bf694f549e868a4530db9560c5bd624
Karen04141 <angel20050401@gmail.com>
2025-11-12 16:53:40 +0800

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,28 +1,18 @@
 import os
-import numpy as np
 
+import numpy
 from numpy.distutils.misc_util import Configuration
 
 
 def configuration(parent_package="", top_path=None):
-    config = Configuration("metrics", parent_package, top_path)
-
+    config = Configuration("cluster", parent_package, top_path)
     libraries = []
     if os.name == "posix":
         libraries.append("m")
-
-    config.add_subpackage("_plot")
-    config.add_subpackage("_plot.tests")
-    config.add_subpackage("cluster")
-
     config.add_extension(
-        "_pairwise_fast", sources=["_pairwise_fast.pyx"], libraries=libraries
-    )
-
-    config.add_extension(
-        "_dist_metrics",
-        sources=["_dist_metrics.pyx"],
-        include_dirs=[np.get_include(), os.path.join(np.get_include(), "numpy")],
+        "_expected_mutual_info_fast",
+        sources=["_expected_mutual_info_fast.pyx"],
+        include_dirs=[numpy.get_include()],
         libraries=libraries,
     )
 
