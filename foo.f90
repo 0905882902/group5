91e6264306b5e3b21724bac4bee57e21d1c4a21a
Karen04141 <angel20050401@gmail.com>
2025-11-12 17:56:50 +0800

    Add files via upload

--- foo.f90
+++ foo.f90
@@ -1,34 +1,20 @@
-module ops_module
 
-  abstract interface
-    subroutine op(x, y, z)
-      integer, intent(in) :: x, y
-      integer, intent(out) :: z
-    end subroutine
-  end interface
 
-contains
+subroutine selectedrealkind(p, r, res)
+  implicit none
+  
+  integer, intent(in) :: p, r
+  !f2py integer :: r=0
+  integer, intent(out) :: res
+  res = selected_real_kind(p, r)
 
-  subroutine foo(x, y, r1, r2)
-    integer, intent(in) :: x, y
-    integer, intent(out) :: r1, r2
-    procedure (op) add1, add2
-    procedure (op), pointer::p
-    p=>add1
-    call p(x, y, r1)
-    p=>add2
-    call p(x, y, r2)
-  end subroutine
-end module
-
-subroutine add1(x, y, z)
-  integer, intent(in) :: x, y
-  integer, intent(out) :: z
-  z = x + y
 end subroutine
 
-subroutine add2(x, y, z)
-  integer, intent(in) :: x, y
-  integer, intent(out) :: z
-  z = x + 2 * y
+subroutine selectedintkind(p, res)
+  implicit none
+
+  integer, intent(in) :: p
+  integer, intent(out) :: res
+  res = selected_int_kind(p)
+
 end subroutine
