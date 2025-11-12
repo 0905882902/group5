4d8b7b02a1f8f1aee52a2e2733b6f6eb3ee3f708
Karen04141 <angel20050401@gmail.com>
2025-11-12 17:36:14 +0800

    Add files via upload

--- __init__.pyi
+++ __init__.pyi
@@ -1,2 +1,13 @@
-# NOTE: The `np._core` namespace is deliberately kept empty due to it
-# being private
+from numpy._core.records import (
+    record as record,
+    recarray as recarray,
+    format_parser as format_parser,
+    fromarrays as fromarrays,
+    fromrecords as fromrecords,
+    fromstring as fromstring,
+    fromfile as fromfile,
+    array as array
+)
+
+__all__: list[str]
+__path__: list[str]
