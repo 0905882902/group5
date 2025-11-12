fb3eea57e14db855cb47e4ad27919e2539026b0c
Karen04141 <angel20050401@gmail.com>
2025-11-12 16:50:00 +0800

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,63 +1,44 @@
-# Author: Alexandre Gramfort <alexandre.gramfort@inria.fr>
-# License: BSD 3 clause
 import os
-
 import numpy
+
+from sklearn._build_utils import gen_from_templates
 
 
 def configuration(parent_package="", top_path=None):
     from numpy.distutils.misc_util import Configuration
 
+    config = Configuration("linear_model", parent_package, top_path)
+
     libraries = []
     if os.name == "posix":
         libraries.append("m")
 
-    config = Configuration("cluster", parent_package, top_path)
-
     config.add_extension(
-        "_dbscan_inner",
-        sources=["_dbscan_inner.pyx"],
-        include_dirs=[numpy.get_include()],
-        language="c++",
-    )
-
-    config.add_extension(
-        "_hierarchical_fast",
-        sources=["_hierarchical_fast.pyx"],
-        language="c++",
-        include_dirs=[numpy.get_include()],
+        "_cd_fast",
+        sources=["_cd_fast.pyx"],
+        include_dirs=numpy.get_include(),
         libraries=libraries,
     )
 
     config.add_extension(
-        "_k_means_common",
-        sources=["_k_means_common.pyx"],
-        include_dirs=[numpy.get_include()],
+        "_sgd_fast",
+        sources=["_sgd_fast.pyx"],
+        include_dirs=numpy.get_include(),
         libraries=libraries,
     )
+
+    # generate sag_fast from template
+    templates = ["sklearn/linear_model/_sag_fast.pyx.tp"]
+    gen_from_templates(templates)
 
     config.add_extension(
-        "_k_means_lloyd",
-        sources=["_k_means_lloyd.pyx"],
-        include_dirs=[numpy.get_include()],
-        libraries=libraries,
+        "_sag_fast", sources=["_sag_fast.pyx"], include_dirs=numpy.get_include()
     )
 
-    config.add_extension(
-        "_k_means_elkan",
-        sources=["_k_means_elkan.pyx"],
-        include_dirs=[numpy.get_include()],
-        libraries=libraries,
-    )
-
-    config.add_extension(
-        "_k_means_minibatch",
-        sources=["_k_means_minibatch.pyx"],
-        include_dirs=[numpy.get_include()],
-        libraries=libraries,
-    )
-
+    # add other directories
     config.add_subpackage("tests")
+    config.add_subpackage("_glm")
+    config.add_subpackage("_glm/tests")
 
     return config
 
