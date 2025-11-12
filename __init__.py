32267869a10a2c786068553b7827697615209a8f
Karen04141 <angel20050401@gmail.com>
2025-11-12 21:02:21 +0800

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,6 +1,1 @@
-from ._dists import Distribution
-from ._envs import Environment
-
-__all__ = ["NAME", "Distribution", "Environment"]
-
-NAME = "importlib"
+"""Contains purely network-related utilities."""
