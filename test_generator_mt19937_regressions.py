49aa52bdb2994af0da83be0fb617d409e4d44da6
Karen04141 <angel20050401@gmail.com>
2025-11-12 17:49:35 +0800

    Add files via upload

--- test_generator_mt19937_regressions.py
+++ test_generator_mt19937_regressions.py
@@ -1,41 +1,34 @@
-import sys
-from numpy.testing import (
-    assert_, assert_array_equal, assert_raises,
-    )
-from numpy import random
+from numpy.testing import (assert_, assert_array_equal)
 import numpy as np
+import pytest
+from numpy.random import Generator, MT19937
 
 
 class TestRegression:
 
-    def test_VonMises_range(self):
+    def setup_method(self):
+        self.mt19937 = Generator(MT19937(121263137472525314065))
+
+    def test_vonmises_range(self):
         # Make sure generated random variables are in [-pi, pi].
         # Regression test for ticket #986.
         for mu in np.linspace(-7., 7., 5):
-            r = random.mtrand.vonmises(mu, 1, 50)
+            r = self.mt19937.vonmises(mu, 1, 50)
             assert_(np.all(r > -np.pi) and np.all(r <= np.pi))
 
     def test_hypergeometric_range(self):
         # Test for ticket #921
-        assert_(np.all(np.random.hypergeometric(3, 18, 11, size=10) < 4))
-        assert_(np.all(np.random.hypergeometric(18, 3, 11, size=10) > 0))
+        assert_(np.all(self.mt19937.hypergeometric(3, 18, 11, size=10) < 4))
+        assert_(np.all(self.mt19937.hypergeometric(18, 3, 11, size=10) > 0))
 
         # Test for ticket #5623
-        args = [
-            (2**20 - 2, 2**20 - 2, 2**20 - 2),  # Check for 32-bit systems
-        ]
-        is_64bits = sys.maxsize > 2**32
-        if is_64bits and sys.platform != 'win32':
-            # Check for 64-bit systems
-            args.append((2**40 - 2, 2**40 - 2, 2**40 - 2))
-        for arg in args:
-            assert_(np.random.hypergeometric(*arg) > 0)
+        args = (2**20 - 2, 2**20 - 2, 2**20 - 2)  # Check for 32-bit systems
+        assert_(self.mt19937.hypergeometric(*args) > 0)
 
     def test_logseries_convergence(self):
         # Test for ticket #923
         N = 1000
-        np.random.seed(0)
-        rvsn = np.random.logseries(0.8, size=N)
+        rvsn = self.mt19937.logseries(0.8, size=N)
         # these two frequency counts should be close to theoretical
         # numbers with this large sample
         # theoretical large N result is 0.49706795
@@ -53,19 +46,18 @@ class TestRegression:
                   [(1, 1), (2, 2), (3, 3), None],
                   [1, (2, 2), (3, 3), None],
                   [(1, 1), 2, 3, None]]:
-            np.random.seed(12345)
-            shuffled = list(t)
-            random.shuffle(shuffled)
-            expected = np.array([t[0], t[3], t[1], t[2]], dtype=object)
+            mt19937 = Generator(MT19937(12345))
+            shuffled = np.array(t, dtype=object)
+            mt19937.shuffle(shuffled)
+            expected = np.array([t[2], t[0], t[3], t[1]], dtype=object)
             assert_array_equal(np.array(shuffled, dtype=object), expected)
 
     def test_call_within_randomstate(self):
-        # Check that custom RandomState does not call into global state
-        m = np.random.RandomState()
-        res = np.array([0, 8, 7, 2, 1, 9, 4, 7, 0, 3])
+        # Check that custom BitGenerator does not call into global state
+        res = np.array([1, 8, 0, 1, 5, 3, 3, 8, 1, 4])
         for i in range(3):
-            np.random.seed(i)
-            m.seed(4321)
+            mt19937 = Generator(MT19937(i))
+            m = Generator(MT19937(4321))
             # If m.state is not honored, the result will change
             assert_array_equal(m.choice(10, size=10, p=np.ones(10)/10.), res)
 
@@ -73,40 +65,49 @@ class TestRegression:
         # Test for multivariate_normal issue with 'size' argument.
         # Check that the multivariate_normal size argument can be a
         # numpy integer.
-        np.random.multivariate_normal([0], [[0]], size=1)
-        np.random.multivariate_normal([0], [[0]], size=np.int_(1))
-        np.random.multivariate_normal([0], [[0]], size=np.int64(1))
+        self.mt19937.multivariate_normal([0], [[0]], size=1)
+        self.mt19937.multivariate_normal([0], [[0]], size=np.int_(1))
+        self.mt19937.multivariate_normal([0], [[0]], size=np.int64(1))
 
     def test_beta_small_parameters(self):
         # Test that beta with small a and b parameters does not produce
         # NaNs due to roundoff errors causing 0 / 0, gh-5851
-        np.random.seed(1234567890)
-        x = np.random.beta(0.0001, 0.0001, size=100)
-        assert_(not np.any(np.isnan(x)), 'Nans in np.random.beta')
+        x = self.mt19937.beta(0.0001, 0.0001, size=100)
+        assert_(not np.any(np.isnan(x)), 'Nans in mt19937.beta')
+
+    def test_beta_very_small_parameters(self):
+        # gh-24203: beta would hang with very small parameters.
+        self.mt19937.beta(1e-49, 1e-40)
+
+    def test_beta_ridiculously_small_parameters(self):
+        # gh-24266: beta would generate nan when the parameters
+        # were subnormal or a small multiple of the smallest normal.
+        tiny = np.finfo(1.0).tiny
+        x = self.mt19937.beta(tiny/32, tiny/40, size=50)
+        assert not np.any(np.isnan(x))
 
     def test_choice_sum_of_probs_tolerance(self):
         # The sum of probs should be 1.0 with some tolerance.
         # For low precision dtypes the tolerance was too tight.
         # See numpy github issue 6123.
-        np.random.seed(1234)
         a = [1, 2, 3]
         counts = [4, 4, 2]
         for dt in np.float16, np.float32, np.float64:
             probs = np.array(counts, dtype=dt) / sum(counts)
-            c = np.random.choice(a, p=probs)
+            c = self.mt19937.choice(a, p=probs)
             assert_(c in a)
-            assert_raises(ValueError, np.random.choice, a, p=probs*0.9)
+            with pytest.raises(ValueError):
+                self.mt19937.choice(a, p=probs*0.9)
 
     def test_shuffle_of_array_of_different_length_strings(self):
         # Test that permuting an array of different length strings
         # will not cause a segfault on garbage collection
         # Tests gh-7710
-        np.random.seed(1234)
 
         a = np.array(['a', 'a' * 1000])
 
         for _ in range(100):
-            np.random.shuffle(a)
+            self.mt19937.shuffle(a)
 
         # Force Garbage Collection - should not segfault.
         import gc
@@ -116,24 +117,24 @@ class TestRegression:
         # Test that permuting an array of objects will not cause
         # a segfault on garbage collection.
         # See gh-7719
-        np.random.seed(1234)
         a = np.array([np.arange(1), np.arange(4)], dtype=object)
 
         for _ in range(1000):
-            np.random.shuffle(a)
+            self.mt19937.shuffle(a)
 
         # Force Garbage Collection - should not segfault.
         import gc
         gc.collect()
 
     def test_permutation_subclass(self):
+
         class N(np.ndarray):
             pass
 
-        np.random.seed(1)
+        mt19937 = Generator(MT19937(1))
         orig = np.arange(3).view(N)
-        perm = np.random.permutation(orig)
-        assert_array_equal(perm, np.array([0, 2, 1]))
+        perm = mt19937.permutation(orig)
+        assert_array_equal(perm, np.array([2, 0, 1]))
         assert_array_equal(orig, np.arange(3).view(N))
 
         class M:
@@ -142,8 +143,23 @@ class TestRegression:
             def __array__(self, dtype=None, copy=None):
                 return self.a
 
-        np.random.seed(1)
+        mt19937 = Generator(MT19937(1))
         m = M()
-        perm = np.random.permutation(m)
-        assert_array_equal(perm, np.array([2, 1, 4, 0, 3]))
+        perm = mt19937.permutation(m)
+        assert_array_equal(perm, np.array([4, 1, 3, 0, 2]))
         assert_array_equal(m.__array__(), np.arange(5))
+
+    def test_gamma_0(self):
+        assert self.mt19937.standard_gamma(0.0) == 0.0
+        assert_array_equal(self.mt19937.standard_gamma([0.0]), 0.0)
+
+        actual = self.mt19937.standard_gamma([0.0], dtype='float')
+        expected = np.array([0.], dtype=np.float32)
+        assert_array_equal(actual, expected)
+
+    def test_geometric_tiny_prob(self):
+        # Regression test for gh-17007.
+        # When p = 1e-30, the probability that a sample will exceed 2**63-1
+        # is 0.9999999999907766, so we expect the result to be all 2**63-1.
+        assert_array_equal(self.mt19937.geometric(p=1e-30, size=3),
+                           np.iinfo(np.int64).max)
