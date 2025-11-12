be7877912a635fa13d49a6d723cd898c325ed9ba
Karen04141 <angel20050401@gmail.com>
2025-11-12 18:04:07 +0800

    Add files via upload

--- __init__.pyi
+++ __init__.pyi
@@ -1,42 +1,28 @@
-import os
-import subprocess
-from collections.abc import Iterable
-from typing import Literal as L, Any, overload, TypedDict
-
 from numpy._pytesttester import PytestTester
 
-class _F2PyDictBase(TypedDict):
-    csrc: list[str]
-    h: list[str]
+from numpy.fft._pocketfft import (
+    fft as fft,
+    ifft as ifft,
+    rfft as rfft,
+    irfft as irfft,
+    hfft as hfft,
+    ihfft as ihfft,
+    rfftn as rfftn,
+    irfftn as irfftn,
+    rfft2 as rfft2,
+    irfft2 as irfft2,
+    fft2 as fft2,
+    ifft2 as ifft2,
+    fftn as fftn,
+    ifftn as ifftn,
+)
 
-class _F2PyDict(_F2PyDictBase, total=False):
-    fsrc: list[str]
-    ltx: list[str]
+from numpy.fft._helper import (
+    fftshift as fftshift,
+    ifftshift as ifftshift,
+    fftfreq as fftfreq,
+    rfftfreq as rfftfreq,
+)
 
 __all__: list[str]
 test: PytestTester
-
-def run_main(comline_list: Iterable[str]) -> dict[str, _F2PyDict]: ...
-
-@overload
-def compile(  # type: ignore[misc]
-    source: str | bytes,
-    modulename: str = ...,
-    extra_args: str | list[str] = ...,
-    verbose: bool = ...,
-    source_fn: None | str | bytes | os.PathLike[Any] = ...,
-    extension: L[".f", ".f90"] = ...,
-    full_output: L[False] = ...,
-) -> int: ...
-@overload
-def compile(
-    source: str | bytes,
-    modulename: str = ...,
-    extra_args: str | list[str] = ...,
-    verbose: bool = ...,
-    source_fn: None | str | bytes | os.PathLike[Any] = ...,
-    extension: L[".f", ".f90"] = ...,
-    full_output: L[True] = ...,
-) -> subprocess.CompletedProcess[bytes]: ...
-
-def get_include() -> str: ...
